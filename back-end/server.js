ed90dee4677f429e8d5ebfb043f322b85e7b3159
itsAiham <aiham_git@gmail.com>
2022-04-22 16:49:43 +0200

    conn db

--- back-end/server.js
+++ back-end/server.js
@@ -12,14 +12,14 @@ const fakser_api = require("./routes/faker");
 
 app.use("/fake", fakser_api);
 
-// mongoose
-//   .connect(process.env.DB_CONNECTION, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() =>
-//     app.listen(PORT, () => signale.start("Start listening on port => " + PORT))
-//   )
-//   .catch((err) => signale.error(err.message));
+mongoose
+  .connect(process.env.DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () => signale.start("Start listening on port => " + PORT))
+  )
+  .catch((err) => signale.error(err.message));
 
-app.listen(PORT, () => signale.start("Start listening on port => " + PORT));
+// app.listen(PORT, () => signale.start("Start listening on port => " + PORT));
