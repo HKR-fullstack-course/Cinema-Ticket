de99d1a9309d4a2b1953ff3ccd33271fa1a8eea5
Quang M Nguyen <quangteoqs15@gmail.com>
2022-04-26 13:43:59 +0200

    Slide component

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,21 @@
 import React from "react";
 import logo from "../img/logo.svg";
 import "../style/App.css";
-
+import FilmSlider from "./Slider"
 import Name from "./Name";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        {/* <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
           Name from Back-end : <Name />
-         
-        </header>
+        </header> */}
+        <FilmSlider />
       </div>
     );
   }
---
+++ src/components/Slider.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "../style/Slider2.css"
+export default class FilmSlider extends Component {
+    render() {
+        const settings = {
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 3,
+            slidesToScroll: 3
+        };
+        return (
+            <div className="container">
+                <h2> Single Item</h2>
+                <Slider {...settings}>
+                    <div>
+                        <h3>movie 1</h3>
+                    </div>
+                    <div>
+                        <h3>movie 2</h3>
+                    </div>
+                    <div>
+                        <h3>movie 3</h3>
+                    </div>
+                    <div>
+                        <h3>movie 4</h3>
+                    </div>
+                    <div>
+                        <h3>movie 5</h3>
+                    </div>
+                </Slider>
+            </div>
+        );
+    }
+}
