6f4d9254acfddabb12df6877a653c1db0b845879
itsAiham <aiham_git@gmail.com>
2022-05-03 12:32:20 +0200

    fix confl

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,35 @@
-import React from "react";
-import "../style/App.css";
-
+import React, { useState } from "react";
+ import Navbar from "./Navbar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "../pages";
+import About from "../pages/about";
+import Movies from "../pages/movies";
+import Signin from "../pages/signin";
+import Services from "../pages/services";
+import Sidebar from "./Sidebar";
 import Parent_slide from "./Parent_Slide";
-class App extends React.Component {
-  render() {
-    return (
-      <div className="App">
-        {/* <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          Name from Back-end : <Name />
-        </header> */}
-        <Parent_slide />
-      </div>
-    );
-  }
+function App() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => {
+    setIsOpen(!isOpen);
+  };
+  return (
+    <Router>
+      this one
+      {/* //NavBar   */}
+      <Navbar toggle={toggle} />
+      <Sidebar isOpen={isOpen} toggle={toggle} />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/movies" element={<Movies />} />
+        <Route path="/signin" element={<Signin />} />
+        {/* //Movie slider  */}
+        <Route path="/movie/:name" element={<Parent_slide />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/d/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import "../style/App.css";
+
+import Parent_slide from "./Parent_Slide";
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        {/* <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          Name from Back-end : <Name />
+        </header> */}
+        <Parent_slide />
+      </div>
+    );
+  }
+}
+
+export default App;
