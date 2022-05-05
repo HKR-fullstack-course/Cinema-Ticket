3d973cfe29d2d6db3dcf1befd94b2a72707012ac
itsAiham <aiham_git@gmail.com>
2022-05-05 17:41:56 +0200

    add signin component

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,46 @@
 import React, { useState } from "react";
-import Navbar from "./Navbar";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+
+import NavbarParent from "./Navbar/NavbarParent";
+import Sidebar from "./Sidebar";
+import Parent_slide from "./Parent_Slide";
+
 import Home from "../pages";
 import About from "../pages/about";
 import Movies from "../pages/movies";
 import Signin from "../pages/signin";
+import ErrorPage from '../pages/ErrorPage'
 import Services from "../pages/services";
-import Sidebar from "./Sidebar";
-import Parent_slide from "./Parent_Slide";
-function App() {
+
+export const AuthContext = React.createContext();
+
+
+const App = () => {
   const [isOpen, setIsOpen] = useState(false);
+
   const toggle = () => {
     setIsOpen(!isOpen);
   };
+
   return (
     <Router>
-      {/* //NavBar   */}
-      <Navbar toggle={toggle} />
+      
+      <NavbarParent  toggle={toggle} />
+
       <Sidebar isOpen={isOpen} toggle={toggle} />
       <Routes>
-        <Route path="/" exact element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/movies" element={<Movies />} />
-        <Route path="/signin" element={<Signin />} />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/about" element={<About />} />
+        <Route exact path="/services" element={<Services />} />
+        <Route exact path="/movies" element={<Movies />} />
+        <Route exact path="/signin" element={<Signin />} />
         {/* //Movie slider  */}
-        <Route path="/movie/:name" element={<Parent_slide />} />
+        <Route exact path="/movie/:name" element={<Parent_slide />} />
+
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
   );
-}
+};
 
 export default App;
--- src/pages/signin.js
+++ src/pages/signin.js
@@ -1,115 +1,15 @@
 import "../style/signin.css";
 import React from "react";
-import { useRef, useState, useEffect, useContext } from "react";
-import AuthContext from "../context/AuthProvider";
+import { useRef, useState, useEffect } from "react";
+import {  Navigate } from "react-router-dom";
 
 import api from "../api/api";
 
-// class Signin extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       username: "",
-//       password: "",
-//     };
 
-//         // const userRef = useRef();
-//         // const errRef = useRef();
+import Auth from "../_helper/Auth";
 
-//         // const [user, setUser] = useState('');
-//         // const [password, setPsw] = useState('');
-//         // const [errMsg, setErrMsg] = useState('');
-//         // const [success, setSeccess] = useState('');
-
-//   }
-
-//   // useEffect(() => {
-//   //   console.log("useEffect has been called!", button);
-//   // });
-
-//   submitLogin = async (e) => {
-//     e.preventDefault();
-//     console.log(this.state.username);
-//     console.log(this.state.password);
-
-//     const response = await api.post(
-//       "/login",
-//       {
-//         email: this.state.username,
-//         password: this.state.password,
-//       }
-//       // JSON.stringify({
-//       //   email: this.state.username,
-//       //   password: this.state.password,
-//       // }),
-//       // {
-//       //   headers: { "Content-Type": "application/json" },
-//       //   withCredentials: true,
-//       // }
-//     );
-
-//     console.log(JSON.stringify(response));
-//   };
-
-//   render() {
-//     return (
-//       <div className="form-login-container">
-//         <div className="header">Login</div>
-//         <div className="box">
-//           <div className="input-group">
-//             <label className="input-label" htmlFor="username">
-//               Email
-//             </label>
-//             <input
-//               type="text"
-//               name="username"
-//               className="login-input"
-//               placeholder=" example@example.com"
-//               autoComplete="off"
-//               value={this.state.username}
-//               onChange={(e) => this.setState({ username: e.target.value })}
-//               required
-//             />
-//           </div>
-
-//           <div className="input-group">
-//             <label className="input-label" htmlFor="password">
-//               Password
-//             </label>
-//             <input
-//               type="password"
-//               name="password"
-//               className="login-input"
-//               placeholder=" Password"
-//               value={this.state.password}
-//               onChange={(e) => this.setState({ password: e.target.value })}
-//               required
-//             />
-//           </div>
-
-//           <p>
-//             <label className="singup-label">Need An Account? </label>
-//             <a href="#">Sing Up</a>
-//           </p>
-
-//           <button
-//             type="button"
-//             className="login-btn"
-//             onClick={this.submitLogin.bind(this)}
-//           >
-//             Sign In
-//           </button>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-import axios from "../api/api";
-const LOGIN_URL = "/auth";
 
 const Signin = () => {
-  const { setAuth } = useContext(AuthContext);
   const userRef = useRef();
   const errRef = useRef();
 
@@ -117,7 +17,7 @@ const Signin = () => {
   const [pwd, setPwd] = useState("");
   const [errMsg, setErrMsg] = useState("");
   const [success, setSuccess] = useState(false);
-
+  
   useEffect(() => {
     userRef.current.focus();
   }, []);
@@ -134,27 +34,18 @@ const Signin = () => {
         "/login",
         { email: user, password: pwd }
 
-        // JSON.stringify({ email: user, password: pwd }),
-        // {
-        //     headers: { 'Content-Type': 'application/json' },
-        //     withCredentials: true
-        // }
       );
-      console.log(response);
-      console.log(JSON.stringify(response?.data));
-      //console.log(JSON.stringify(response));
-      const accessToken = response?.data?.accessToken;
-      console.log(accessToken);
-      const roles = response?.data?.roles;
-      setAuth({ user, pwd, roles, accessToken });
-      setUser("");
-      setPwd("");
+
+      const accessToken = response?.data?.token;
+      window.localStorage.setItem("auth-token", accessToken);
       setSuccess(true);
+      // Auth.login(accessToken)
+
     } catch (err) {
       if (!err?.response) {
         setErrMsg("No Server Response");
       } else if (err.response?.status === 400) {
-        setErrMsg("Missing Email or Password");
+        setErrMsg("Check Email and Password!");
       } else if (err.response?.status === 401) {
         setErrMsg("Unauthorized");
       } else {
@@ -167,13 +58,9 @@ const Signin = () => {
   return (
     <>
       {success ? (
-        <section>
-          <h1>You are logged in!</h1>
-          <br />
-          <p>
-            <a href="#">Go to Home</a>
-          </p>
-        </section>
+        <Navigate replace to="/" >
+          {window.location.replace('/')}
+        </Navigate>
       ) : (
         <section className="form-login-container">
           <h1 className="header">Sign In</h1>
@@ -217,7 +104,7 @@ const Signin = () => {
             Need an Account?
             <span className="line">
               {/*put router link here*/}
-              <a href="#"> Sign Up</a>
+              <a href="/"> Sign Up</a>
             </span>
           </p>
         </section>
