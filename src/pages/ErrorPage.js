e3d708885e3121f75b9898d35cc8de08479bfab3
itsAiham <aiham_git@gmail.com>
2022-05-05 17:44:38 +0200

    add 404-page

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ErrorPage = () => {
+  return (
+    <>
+      Page Not Found!! <a href="/"> Click To Return </a>
+    </>
+  );
+};
+
+export default ErrorPage;
