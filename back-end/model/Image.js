eb95a3bf8da76a595ad1d6e11ae703cb269f43b4
itsAiham <aiham_git@gmail.com>
2022-05-07 18:23:36 +0200

    add image-post fun to test posting from client side

--- back-end/model/Image.js
+++ back-end/model/Image.js
@@ -2,17 +2,30 @@ const mongoose = require("mongoose");
 
 const imageSchema = new mongoose.Schema(
   {
-    img: { 
-        data: Buffer, 
-        contentType: String, 
- },
-    owner_id: {
-      // to save img-owner id e.g. movie or user
-      type: String,
-      required: true,
+    img: {
+      data: String,
+      contentType: String,
     },
+    // owner_id: {
+    //   // to save img-owner id e.g. movie or user
+    //   type: String,
+    //   required: true,
+    // },
   },
   { timestamps: true }
+
+  //   {
+  //     img: {
+  //         data: Buffer,
+  //         contentType: String,
+  //  },
+  //     owner_id: {
+  //       // to save img-owner id e.g. movie or user
+  //       type: String,
+  //       required: true,
+  //     },
+  //   },
+  //   { timestamps: true }
 );
 
 module.exports = new mongoose.model("Image", imageSchema);
