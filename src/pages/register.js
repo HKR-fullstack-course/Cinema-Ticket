fb3dfa8c95583115cf80c93393287edffc1550e0
itsAiham <aiham_git@gmail.com>
2022-05-09 21:20:10 +0200

    update user after register

--- src/pages/register.js
+++ src/pages/register.js
@@ -21,16 +21,21 @@ const Register = () => {
   const [success, setSuccess] = useState(false);
 
   const [image, setImage] = useState("");
-  const [url, setURL] = useState("");
   const [user_id, setUserID] = useState("");
 
-  const updateImageInDB = async () => {
-    try {
-      await api.update("", {
-        _id: user_id,
-        image: url + user_id,
-      });
-    } catch (error) {}
+  const updateImageInDB = async (url) => {
+    console.log("url : ", url);
+    console.log("id > ", user_id);
+    if (url) {
+      try {
+        await api.put("/update_user", {
+          _id: "627963a230ab0100309773ff",
+          image: url,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    }
   };
 
   // const updateUserAvatar = async () => {
@@ -98,7 +103,9 @@ const Register = () => {
             `/image/avatar/default`,
         })
 
-        .then((res) => {
+        .then(async (res) => {
+          setUserID(res.data.user_id);
+          let reqImg;
           if (image) {
             const formData = new FormData();
             formData.append("file", image);
@@ -106,7 +113,7 @@ const Register = () => {
             formData.append("cloud_name", process.env.REACT_APP_CLOUD_NAME);
             formData.append("public_id", res.data.user_id);
 
-            const x = fetch(
+            reqImg = await fetch(
               `https://api.cloudinary.com/v1_1/` +
                 process.env.REACT_APP_CLOUD_NAME +
                 `/image/upload`,
@@ -115,8 +122,26 @@ const Register = () => {
                 body: formData,
               }
             );
+            // console.log(x.value);
+            // console.log(x.json());
           }
+          // console.log("id > " ,user_id);
+          return reqImg.json();
+        })
+        .then(async (value) => {
+          // console.log(value.secure_url);
+          // updateImageInDB(value.secure_url);
+
+          await api.put("/update_user", {
+            _id: user_id,
+            image_url: value.secure_url,
+          });
         });
+
+      // .then(data => {
+      //   console.log(data);
+      //   console.log(data.json());
+      // })
     } catch (err) {
       if (!err?.response) {
         setErrMsg("No Server Response");
