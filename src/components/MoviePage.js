c761c7dc2b318e4a901a624955eb673e5f1d8f73
Quang M Nguyen <quangteoqs15@gmail.com>
2022-05-11 02:33:52 +0200

    Update movie page

--- src/components/MoviePage.js
+++ src/components/MoviePage.js
@@ -7,42 +7,33 @@ import api from "../api/api"
 const MoviePage = () => {
     const { _id } = useParams()
     const [movie, setMove] = useState({ data: [], })
-    const [time, setMovieTime] = useState({ time: [] })
+    const [time, setMovieTime] = useState([])
+    const [price, setMoviePrice] = useState({ price: '' })
+    const [people, setMoviePeoples] = useState({ people: '' })
+    const [finalPrice, setMovieFinalPrice] = useState('')
 
-    const countries = [
-
-        { label: "Kenya", value: "Kenya", id: 1 },
-        { label: "India", value: "India", id: 2 },
-        { label: "Botswana", value: "Botswana", id: 3 }
-    ]
-
-    const [countryReg, setCountryReg] = useState('')
-
-    const handleCountryChange = (e) => {
-        setCountryReg(e.target.id)
-
-
+    const handlePriceChange = (e) => {
+        setMoviePrice({ price: e.target.value })
+    }
+    const handleNumPeoplesChange = (e) => {
+        const result = e.target.value.replace(/\D/g, '');
+        setMoviePeoples({ people: result })
     }
 
+    const totalCost = (a, b) => {
+        return a * b
+    }
+    const handleFinalCostChange = () => {
+        setMovieFinalPrice(totalCost((price.price), (people.people)));
+    }
 
     useEffect(() => {
         api.get(`/movie/${_id}`)
-            .then(response => setMove({ data: response.data.body, time: response.data.body.time }))
+            .then(response => { setMove({ data: response.data.body }); setMovieTime(response.data.body.time) })
     }, [_id]);
 
     return (
         <div>
-            <div className="App">
-                {countryReg}
-                <br />
-                <select onChange={handleCountryChange}>
-                    <option value=""> -- Select a Country -- </option>
-
-                    {countries.map((countryReg) => <option value={countryReg.value}>{countryReg.label}</option>)}
-                </select>
-
-            </div>
-
             <div className={styles.container}>
                 <h3>{movie.data.name}</h3>
                 <h5>Genre: {movie.data.movie_type}</h5>
@@ -52,11 +43,23 @@ const MoviePage = () => {
                 <h5>Description: {movie.data.description}</h5>
                 <h5>Movie budget: {movie.data.budget} million dollars</h5>
                 <h5>Actors: {[movie.data.main_actors].toString()}</h5>
-                {movie.time && movie.time.map((time) => new Map([]))}
-                <h3>Buy ticket</h3>
+                <h3>Select a time</h3>
                 <div>
-                    <button >Book ticket</button>
+                    <div className="drop_down">
+                        <select onChange={handlePriceChange}>
+                            <option value="">Pick a date</option>
+                            {time.map((time) =>
+                                <option value={time.ticket_price} key={time._id}>
+                                    {time.show_time}
+                                </option>)}
+                        </select>
+                    </div>
+                    <input type="text" placeholder="Number of ticket" value={people.people} onChange={handleNumPeoplesChange} />
+                    <button onClick={handleFinalCostChange}>
+                        Book ticket
+                    </button >
                 </div>
+                <p>Total amount to pay {finalPrice}kr</p>
 
 
             </div>
